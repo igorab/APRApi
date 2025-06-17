e6fb9988526ce60d5ca07959274bba28c5990e34
Balakirev Igor <igor.balakirev@mail.ru>
2025-06-17 18:29:21 +0300

    idx

--- Program.cs
+++ Program.cs
@@ -1,55 +1,169 @@
+using System;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
+// начальные данные
+List<Person> users = new List<Person>
+{
+    new() { Id = Guid.NewGuid().ToString(), Name = "Tom", Age = 37 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Bob", Age = 41 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Sam", Age = 24 }
+};
+
 
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 
-//https://metanit.com/sharp/aspnet6/2.10.php
-
-Task ReadData(HttpContext сontext)
+app.Run(async (context) => 
 {
-    
-    return null;
-}
+    var response = context.Response;
+    var request = context.Request;
+    var path = request.Path;
 
-RequestDelegate handler = ReadData;
+    string expressionForGuid = @"^/api/users/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$";
 
-app.Run(ReadData);
+    if (path == "/api/users" && request.Method == "GET")
+    {
+        await GetAllPeople(response);
+    }
+    else if (Regex.IsMatch(path, expressionForGuid) && request.Method == "GET")
+    {
+        // получаем id из адреса url
+        string? id = path.Value?.Split("/")[3];
+        await GetPerson(id, response);
+    }
+    else if (path == "/api/users" && request.Method == "POST")
+    {
+        await CreatePerson(response, request);
+    }
+    else if (path == "/api/users" && request.Method == "PUT")
+    {
+        await UpdatePerson(response, request);
+    }
+    else if (Regex.IsMatch(path, expressionForGuid) && request.Method == "DELETE")
+    {
+        string? id = path.Value?.Split("/")[3];
+        await DeletePerson(id, response);
+    }
+    else
+    {
+        response.ContentType = "text/html; charset=utf-8";
+        await response.SendFileAsync("html/index.html");
+    }
+
+
+});
 
 app.Run();
 
+// получение всех пользователей
+async Task GetAllPeople(HttpResponse response)
+{
+    await response.WriteAsJsonAsync(users);
+}
 
-//var builder = WebApplication.CreateSlimBuilder(args);
+// получение одного пользователя по id
+async Task GetPerson(string? id, HttpResponse response)
+{
+    // получаем пользователя по id
+    Person? user = users.FirstOrDefault((u) => u.Id == id);
+    // если пользователь найден, отправляем его
+    if (user != null)
+        await response.WriteAsJsonAsync(user);
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+    }
+}
 
-//builder.Services.ConfigureHttpJsonOptions(options =>
-//{
-//options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-//});
+async Task DeletePerson(string? id, HttpResponse response)
+{
+    // получаем пользователя по id
+    Person? user = users.FirstOrDefault((u) => u.Id == id);
+    // если пользователь найден, удаляем его
+    if (user != null)
+    {
+        users.Remove(user);
+        await response.WriteAsJsonAsync(user);
+    }
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    else
+    {
+        response.StatusCode = 404;
+        await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+    }
+}
 
-//WebApplication app = builder.Build();
+async Task CreatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        // получаем данные пользователя
+        var user = await request.ReadFromJsonAsync<Person>();
+        if (user != null)
+        {
+            // устанавливаем id для нового пользователя
+            user.Id = Guid.NewGuid().ToString();
+            // добавляем пользователя в список
+            users.Add(user);
+            await response.WriteAsJsonAsync(user);
+        }
+        else
+        {
+            throw new Exception("Некорректные данные");
+        }
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Некорректные данные" });
+    }
+}
 
-//Todo[] sampleTodos = new Todo[] {
-//    new(1, "Walk the dog"),
-//    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-//    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-//    new(4, "Clean the bathroom"),
-//    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-//};
+async Task UpdatePerson(HttpResponse response, HttpRequest request)
+{
+    try
+    {
+        // получаем данные пользователя
+        Person? userData = await request.ReadFromJsonAsync<Person>();
+        if (userData != null)
+        {
+            // получаем пользователя по id
+            var user = users.FirstOrDefault(u => u.Id == userData.Id);
+            // если пользователь найден, изменяем его данные и отправляем обратно клиенту
+            if (user != null)
+            {
+                user.Age = userData.Age;
+                user.Name = userData.Name;
+                await response.WriteAsJsonAsync(user);
+            }
+            else
+            {
+                response.StatusCode = 404;
+                await response.WriteAsJsonAsync(new { message = "Пользователь не найден" });
+            }
+        }
+        else
+        {
+            throw new Exception("Некорректные данные");
+        }
+    }
+    catch (Exception)
+    {
+        response.StatusCode = 400;
+        await response.WriteAsJsonAsync(new { message = "Некорректные данные" });
+    }
+}
 
-//var todosApi = app.MapGroup("/todos");
-//todosApi.MapGet("/", () => sampleTodos);
-//todosApi.MapGet("/{id}", (int id) =>
-//    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-//        ? Results.Ok(todo)
-//        : Results.NotFound());
 
-//app.Run();
 
-////app.UseWelcomePage();
 
-//public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
-//[JsonSerializable(typeof(Todo[]))]
-//internal partial class AppJsonSerializerContext : JsonSerializerContext
-//{
-
-//}
+public class Person
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Age { get; set; }    
+}
